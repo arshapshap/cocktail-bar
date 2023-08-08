1174c17e552d56e45df894844029134d5d831993
Artyom Shapovalov <arshapshap@gmail.com>
2023-08-08 10:35:54 +0300

    Add base navigation

---
+++ app/src/main/java/com/arshapshap/surftraineetask/presentation/screens/ListFragment.kt
@@ -0,0 +1,8 @@
+package com.arshapshap.surftraineetask.presentation.screens
+
+import androidx.fragment.app.Fragment
+import com.arshapshap.surftraineetask.R
+
+class ListFragment : Fragment(R.layout.fragment_list) {
+
+}
